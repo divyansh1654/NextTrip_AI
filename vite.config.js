c9c723d54b9e0296a7f3f868942b3be6e86f7ac1
divyansh1654 <123314515+divyansh1654@users.noreply.github.com>
2025-02-13 22:58:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000 // Set the limit to 1000 KB (1MB)
+  },
   plugins: [react()],
   resolve: {
     alias: {
